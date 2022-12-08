222e8b3aa12a62587e22cf1340d1be7cd3129088
Kyle - Laptop <kyle.volle@gmail.com>
2022-12-08 12:06:14 -0600

    Now Odometry_toPoseStamped can be a standalone node
    This node listens to an odom topic from gazebo and publishes a PoseStamped, matching mocap

--- scripts/Odometry_to_PoseStamped.py
+++ scripts/Odometry_to_PoseStamped.py
@@ -8,14 +8,20 @@ from geometry_msgs.msg import PoseStamped
 
 class odometry_to_posestamped():
     def __init__(self):
-
+        self.pose_msg = PoseStamped()
         self.pose_pub = rospy.Publisher("/pose_stamped", PoseStamped, queue_size=50)
         rospy.Subscriber("/multirotor/truth/NED", Odometry, self.Odometry_callback)
 
-
     def Odometry_callback(self, odom_msg):
-        pose_msg = PoseStamped()
-        pose_msg.header = odom_msg.header
-        pose_msg.pose = odom_msg.pose.pose
+        self.pose_msg.header = odom_msg.header
+        self.pose_msg.pose = odom_msg.pose.pose
 
-        self.pose_pub.publish(pose_msg)
+        self.pose_pub.publish(self.pose_msg)
+
+
+if __name__ == "__main__":
+    rospy.init_node('odom_to_pose_stamped')
+    otp = odometry_to_posestamped()
+    rospy.spin()
+
+ 
